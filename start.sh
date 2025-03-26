4d3560dc079a3145eaffa1fff1561c3c1d4d4358
Richard Hennessey III <rhennessey3@gmail.com>
2025-03-26 12:07:41 -0400

    Add environment variables debugging script

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+# Debug environment variables
+echo "=== Debugging environment variables ==="
+node debug-env.js
+
 # Start the application
+echo "=== Starting server ==="
 node dist/server.js
